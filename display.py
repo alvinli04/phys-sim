637393f02af7cbeafa648ee29dae98da111a4bff
alvinli04 <alvin.li.j@gmail.com>
2022-06-01 10:14:50 -0400

    some progress

--- display.py
+++ display.py
@@ -8,12 +8,12 @@ pendulum_box.height = 850
 pendulum_box.range = 1.3
 pendulum_box.title = "<h1>Double Pendulum</h1>"
 
-gg = graph(title = '<b>Phase Diagram</b>', width=750, height=500, xmin = -2*pi, xmax = 2*pi, align = 'left', xtitle='Angle', ytitle='Angular Velocity (rad/s)', ymin = -10, ymax=10)
+gg = graph(title = '<b>Phase Diagram</b>', width=750, height=500, xmin = -2*pi, xmax = 2*pi,
+            align = 'left', xtitle='Angle', ytitle='Angular Velocity (rad/s)', ymin = -10, ymax=10, scroll = True)
 
 # test curve
-f1 = gcurve(color=color.cyan) # a graphics curve
-
-
+f1 = gcurve(color=color.cyan, dot = True, dot_color = color.red) # a graphics curve
+f2 = gcurve(color=color.green, dot = True, dot_color = color.purple)
 
 box_object = box(canvas = pendulum_box)
 
@@ -29,6 +29,11 @@ l2 = length2.value
 m = mratio.value
 
 while True:
+    rate(500)
     angle1, angle2, w1, w2 = step(angle1,angle2,w1,w2,l1,l2,1,m)
     f1.plot(pos=(angle1,w1))
-    print(angle1, w1)
+    f2.plot(pos=(angle2,w2))
+    if len(f1.data) > 2500:
+        f1.data.pop(0)
+
+    # print(angle1, w1)
