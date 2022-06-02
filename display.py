2f113cc48f02a03071d17c8a9e7bbe69746a6d88
Rishabh Das <rdas20@stuy.edu>
2022-06-02 09:25:07 -0400

    Wrapping works

--- display.py
+++ display.py
@@ -8,8 +8,8 @@ pendulum_box.height = 850
 pendulum_box.range = 1.3
 pendulum_box.title = "<h1>Double Pendulum</h1>"
 
-gg = graph(title = '<b>Phase Diagram</b>', width=750, height=500, xmin = -pi, xmax = pi,
-            align = 'left', xtitle='Angle', ytitle='Angular Velocity (rad/s)', ymin = -10, ymax=10, scroll = True)
+gg = graph(title = '<b>Phase Diagram</b>', width=750, height=500, xmin = -3, xmax = 3,
+            align = 'left', xtitle='Angle (rad)', ytitle='Angular Velocity (rad / s)', ymin = -10, ymax=10, scroll = False)
 
 # test curve
 f1 = gcurve(color=color.cyan, dot = True, dot_color = color.red) # a graphics curve
@@ -31,11 +31,20 @@ m = mratio.value
 while True:
     rate(500)
     angle1, angle2, w1, w2 = step(angle1,angle2,w1,w2,l1,l2,1,m)
-    a = angle1-2*pi*floor((angle1+pi)/(2*pi))
-    b = angle2-2*pi*floor((angle2+pi)/(2*pi))
-    f1.plot(pos=(a,w1))
-    f2.plot(pos=(b,w2))
-    if len(f1.data) > 2500:
-        f1.data.pop(0)
+    if angle1 > pi:
+        angle1 -= 2*pi
+        f1 = gcurve(color=color.cyan, dot = True, dot_color = color.red)
+    if angle1 < -pi:
+        angle1 += 2*pi
+        f1 = gcurve(color=color.cyan, dot = True, dot_color = color.red)
+    if angle2 > pi:
+        angle2 -= 2*pi
+        f2 = gcurve(color=color.green, dot = True, dot_color = color.purple)
+    if angle2 < -pi:
+        angle2 += 2*pi
+        f2 = gcurve(color=color.green, dot = True, dot_color = color.purple)
+    f1.plot(pos=(angle1,w1))
+    f2.plot(pos=(angle2,w2))
+
 
     # print(angle1, w1)
